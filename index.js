34dd5b8a9177d17d3ea13909b423fe6383745aed
Oscar <oshardy3@gmail.com>
2015-07-09 09:29:16 -0500

    index.js: Namespace cambiado.

--- index.js
+++ index.js
@@ -4,38 +4,40 @@ var io = require('socket.io')(http);
 var bodyParser = require('body-parser');  // Parser para leer querys tipo POST.
 app.use( bodyParser. urlencoded() );  // Implementación del parser para la app.
 
-var validos = { // JSON con el usuario y contraseña para loggearse.
+var valido = { // Objeto con el usuario y contraseña para loggearse.
   user: "usuario",
   pass: "password"
 };
-
+//Routes
 app.get('/', function(req, res){  // Raíz para el chat del cliente.
   res.sendFile(__dirname + '/cliente.html');
 });
 app.get('/chat', function (req, res){ // Chat solicitado por el cliente.
   res.sendFile(__dirname + '/chat.html');
 });
-
 app.get('/login', function (req, res) { // Login para el admin.
   res.sendFile(__dirname + '/login.html')
 });
 app.post('/salas', function (req, res) { // Salas activas visibles para
   var user = req.body.usuario;          // el admin.
   var pass = req.body.password;
-  if (user === validos.user && pass === validos.pass) { // Condicional de
+  if (user === valido.user && pass === valido.pass) { // Condicional de
     res.sendFile(__dirname + '/salas.html');            // logeo exitoso.
   }
   else {
     res.sendFile(__dirname + '/badlogin.html'); // Respuesta en caso de error.
   }
 });
-
-io.on('connection', function(socket){
+//Socket
+var nsp = io.of('/consultas');
+nsp.on('connection', function(socket){
+  console.log('Conectado en' + nsp );
   socket.on('Mensaje', function(msg){
-    io.emit('Mensaje', msg);  // Emisión del mensaje en el cliente web.
+    socket.join('/consultas');
+    nsp.to('/consultas').emit('Mensaje', msg);  // Emisión del mensaje en el cliente web.
   });
 });
-
+//Server
 http.listen(3000, function(){ // Puerto a usar en el server.
   console.log('listening on *:3000');
 });
